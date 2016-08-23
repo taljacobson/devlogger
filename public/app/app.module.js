92e1c6b485c989f83eb79e505eeffb76a2d7c494
taljacobson <tal1989@gmail.com>
2016-08-23 13:59:56 +0300

    module chagense

--- public/app/app.module.js
+++ public/app/app.module.js
@@ -10,14 +10,29 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 };
 var core_1 = require('@angular/core');
 var platform_browser_1 = require('@angular/platform-browser');
+var forms_1 = require('@angular/forms');
+var http_1 = require('@angular/http');
+var feathers_service_1 = require("./services/feathers.service");
 var app_component_1 = require('./components/app/app.component');
+var logs_component_1 = require("./components/logs/logs.component");
 var AppModule = (function () {
     function AppModule() {
     }
     AppModule = __decorate([
         core_1.NgModule({
-            imports: [platform_browser_1.BrowserModule],
-            declarations: [app_component_1.AppComponent],
+            imports: [
+                platform_browser_1.BrowserModule,
+                http_1.HttpModule,
+                forms_1.FormsModule
+            ],
+            declarations: [
+                app_component_1.AppComponent,
+                logs_component_1.LogsComponent
+            ],
+            providers: [
+                feathers_service_1.FeathersService,
+                http_1.HTTP_PROVIDERS
+            ],
             bootstrap: [app_component_1.AppComponent]
         }), 
         __metadata('design:paramtypes', [])
--- public/app/app.module.ts
+++ public/app/app.module.ts
@@ -1,9 +1,29 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule }    from '@angular/forms';
+import { HttpModule,HTTP_PROVIDERS }     from '@angular/http';
+
+import { FeathersService } from "./services/feathers.service";
+
 import { AppComponent }  from './components/app/app.component';
+import { LogsComponent } from "./components/logs/logs.component";
+
+
+
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+  imports:      [
+    BrowserModule,
+    HttpModule,
+    FormsModule
+   ],
+  declarations: [
+    AppComponent,
+    LogsComponent
+  ],
+  providers: [
+    FeathersService,
+    HTTP_PROVIDERS
+  ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
