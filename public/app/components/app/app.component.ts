953bcb6a5367ab2d1116ae3235866afddbb70174
taljacobson <tal1989@gmail.com>
2016-08-23 12:12:46 +0300

    ng2 component init

---
+++ public/app/components/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+@Component({
+  moduleId: module.id,
+  selector: 'my-app',
+  templateUrl: './app.component.html'
+})
+
+export class AppComponent {
+  constructor(){
+
+  }
+}
